d2e718777cead351f20c4b06df6e24170471b957
rahul <abc@gmail.com>
2022-03-08 15:56:35 +0530

    first commit by laptop

---
+++ src/test/java/gitproject1/AutomationTest.java
@@ -0,0 +1,18 @@
+package gitproject1;
+
+import org.testng.annotations.Test;
+
+public class AutomationTest {
+	@Test
+	public void selenium() {
+		System.out.println("this is selenium");
+	}
+	@Test
+	public void appium() {
+		System.out.println("this is appium");
+	}
+	@Test
+	public void restAssured() {
+		System.out.println("this is restassured");
+	}
+}
---
+++ src/test/java/gitproject1/ManualTest.java
@@ -0,0 +1,21 @@
+package gitproject1;
+
+import org.testng.annotations.Test;
+
+public class ManualTest {
+	@Test
+	public void unit() {
+		System.out.println("this is unit testing");
+		System.out.println("this is unit testing1");
+		System.out.println("this is unit testing2");
+	}
+	@Test
+	public void integression() {
+		System.out.println("this is integression testing");
+	}
+	@Test
+	public void functional() {
+		System.out.println("this is functional testing");
+	}
+
+}
