d15e298bc740aaeeea8e44947e70d8c410ffb669
rahulrahul <ab@gmail.com>
2022-03-08 16:36:24 +0530

    second commit by pc

--- src/test/java/gitproject1/AutomationTest.java
+++ src/test/java/gitproject1/AutomationTest.java
@@ -6,6 +6,8 @@ public class AutomationTest {
 	@Test
 	public void selenium() {
 		System.out.println("this is selenium");
+		System.out.println("this is selenium1");
+		System.out.println("this is selenium2");
 	}
 	@Test
 	public void appium() {
